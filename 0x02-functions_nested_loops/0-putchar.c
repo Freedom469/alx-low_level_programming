b37c4c30e66c18394337831c6497462c1891e1f0
Malik <bamjnr99@gmail.com>
2022-09-13 06:22:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - first entry point
+ *
+ *
+ *
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char c[8] = "_putchar"
+	while (c < 7)
+	{
+		_putchar(c);
+		c++;
+	}
+_putchar('\n');
+return (0);
+}
