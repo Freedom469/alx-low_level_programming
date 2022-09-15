08888bfe2c37bb62a694fa70d44622f763fdb813
Malik <bamjnr99@gmail.com>
2022-09-15 06:33:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void positive_or_negative(int i);
+void print_most_numbers(void);
+void more_numbers(void);
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int, int);
+
+
+
+#endif /* MAIN_H */ 
