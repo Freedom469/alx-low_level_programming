fbcfea8758c0b9d97726d78b836751ba183af288
Malik <bamjnr99@gmail.com>
2022-09-15 06:41:33 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - func name
+ *
+ */
+
+void more_numbers(void)
+{
+int a = 0;
+int b;
+while (a <= 10)
+{
+for (b = 0; b <= 14; b++)
+{
+_putchar(b);
+}
+	a++;
+	_putchar('\n');
+}
+_putchar('\n');
+}
