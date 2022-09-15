23d247918e5b173f6587442fd1efbe67c292a03c
Malik <bamjnr99@gmail.com>
2022-09-15 09:44:59 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,13 +7,8 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void positive_or_negative(int i);
-void print_most_numbers(void);
-void more_numbers(void);
-int largest_number(int, int, int);
-
-void print_remaining_days(int, int, int);
-
-int convert_day(int, int);
+void print_most_numbers;	
+void print_line(int n);
 
 
 
