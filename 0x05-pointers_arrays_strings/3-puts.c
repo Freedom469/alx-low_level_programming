73ea4f393c09fcdfad55cdb39b7611471e74ae78
Malik <bamjnr99@gmail.com>
2022-09-19 09:39:15 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -14,12 +14,13 @@ void _puts(char *str)
 
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
+		if (*(str + count) != '\0')
 		{
-			_putchar('\n');
-			break;
+			count++;
 		}
 		_putchar(str[count]);
-		count++;
+
 	}
+
+	_putchar('\n');
 }
