80b263ab37758793cb36842c89dc7aef030b905e
Malik <bamjnr99@gmail.com>
2022-09-19 13:42:51 -0700

    puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,11 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+int _putchar(char c);
 
 #endif
