eb7c50b1552a7223181fb1a7ba042a2bc6c91232
Malik <bamjnr99@gmail.com>
2022-09-21 04:52:09 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_strncpy - copies a string
+ *
+ * @dest: destination file
+ *
+ * @src: source file
+ *
+ * @n: int variable
+ *
+ * Return: dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		while (i < n)
+		{
+			dest[i] = src[i];
+
+			i++;
+		}
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
