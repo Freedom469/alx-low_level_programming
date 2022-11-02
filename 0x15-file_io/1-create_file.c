b7848d06c0e59c86b3d9e6481d4e12f18cbd8f07
Malik <bamjnr99@gmail.com>
2022-11-02 14:47:16 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-
-
 /**
-* create_file - fuction that creates a file
-* @filename: pointer to the file to be created
-* @text_content: pointer to NULL terminated string to write to the file
-* Return: NULL terminated string to write to the file
-*/
+ * create_file - fuction that creates a file
+ * @filename: pointer to the file to be created
+ * @text_content: pointer to NULL terminated string to write to the file
+ * Return: NULL terminated string to write to the file
+ */
 
 int create_file(const char *filename, char *text_content)
 
@@ -15,26 +13,54 @@ int create_file(const char *filename, char *text_content)
 
 	int fd;
 
+	int nletters;
+
+	int rwr;
+
 
 
 	if (!filename)
 
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fd != -1)
 
-	{
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-		write(fd, text_content, sizeof(text_content));
 
-		return (1);
 
-	}
+	if (fd == -1)
+
+		return (-1);
+
+
+
+	if (!text_content)
+
+		text_content = "";
+
+
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+
+		;
+
+
+
+	rwr = write(fd, text_content, nletters);
+
+
+
+	if (rwr == -1)
+
+		return (-1);
+
+
 
 	close(fd);
 
-	return (-1);
+
+
+	return (1);
 
 }
